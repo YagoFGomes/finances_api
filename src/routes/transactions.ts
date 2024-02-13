68bdea56de38b0d9ba533d5963677c146ebaed3a
Yago Ferreira <yago.fgomes@gmail.com>
2024-02-13 17:09:35 +0000

    ajustando o serve para os testes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,103 +5,81 @@ import { z } from 'zod'
 import { checkSessionidExists } from '../middlewares/checkSessionIdExists'
 
 export async function transactionRoutes(app: FastifyInstance) {
-  app.get(
-    '/',
-    {
-      preHandler: [checkSessionidExists],
-    },
-    async (request) => {
-      const { sessionId } = request.cookies
+  app.addHook('preHandler', checkSessionidExists)
 
-      const transactions = await knex('transactions')
-        .where('session_id', sessionId)
-        .select()
+  app.get('/', async (request) => {
+    const { sessionId } = request.cookies
 
-      return { transactions }
-    },
-  )
+    const transactions = await knex('transactions')
+      .where('session_id', sessionId)
+      .select()
 
-  app.get(
-    '/:id',
-    {
-      preHandler: [checkSessionidExists],
-    },
-    async (request) => {
-      const { sessionId } = request.cookies
+    return { transactions }
+  })
 
-      const getTransactionParamsSchema = z.object({
-        id: z.string().uuid(),
-      })
+  app.get('/:id', async (request) => {
+    const { sessionId } = request.cookies
 
-      const { id } = getTransactionParamsSchema.parse(request.params)
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
 
-      const transaction = await knex('transactions')
-        .where({
-          id,
-          session_id: sessionId,
-        })
-        .first()
+    const { id } = getTransactionParamsSchema.parse(request.params)
 
-      return { transaction }
-    },
-  )
-
-  app.get(
-    '/summary',
-    {
-      preHandler: [checkSessionidExists],
-    },
-    async (request) => {
-      const { sessionId } = request.cookies
-
-      const summary = await knex('transactions')
-        .where('session_id', sessionId)
-        .sum('amount', { as: 'ammount' })
-        .first()
-
-      return { summary }
-    },
-  )
-
-  app.post(
-    '/',
-    {
-      preHandler: [checkSessionidExists],
-    },
-    async (request, reply) => {
-      // criando um tipo para o body com o zod
-      const createTransactionBodySchema = z.object({
-        title: z.string(),
-        amount: z.number(),
-        type: z.enum(['credit', 'debit']),
-      })
-
-      // verificando o request.body com o tipo criado
-      const { amount, title, type } = createTransactionBodySchema.parse(
-        request.body,
-      )
-      // caso de erro o zod irá retornar uma exception
-
-      // criando cookies para identificar um usuário sem nem mesmo ter um login
-      let sessionId = request.cookies.sessionId
-
-      if (!sessionId) {
-        sessionId = randomUUID()
-        reply.cookie('sessionId', sessionId, {
-          path: '/',
-          maxAge: 60 * 60 * 24 * 7, // 7 dias
-        })
-      }
-
-      // criando a transação com o knex
-      await knex('transactions').insert({
-        id: randomUUID(),
-        title,
-        amount: type === 'credit' ? amount : amount * -1,
+    const transaction = await knex('transactions')
+      .where({
+        id,
         session_id: sessionId,
       })
+      .first()
 
-      return reply.status(201).send()
-    },
-  )
+    return { transaction }
+  })
+
+  app.get('/summary', async (request) => {
+    const { sessionId } = request.cookies
+
+    const summary = await knex('transactions')
+      .where('session_id', sessionId)
+      .sum('amount', { as: 'ammount' })
+      .first()
+
+    return { summary }
+  })
+
+  app.post('/', async (request, reply) => {
+    // criando um tipo para o body com o zod
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    // verificando o request.body com o tipo criado
+    const { amount, title, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+    // caso de erro o zod irá retornar uma exception
+
+    // criando cookies para identificar um usuário sem nem mesmo ter um login
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 dias
+      })
+    }
+
+    // criando a transação com o knex
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
+    })
+
+    return reply.status(201).send()
+  })
 }
